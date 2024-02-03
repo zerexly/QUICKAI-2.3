347fd48dc6137f290966f80c5f1a7a1c0d3ef557
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-04 00:04:48 +0530

    Update ai-templates.php

--- php/app/ai-templates.php
+++ php/app/ai-templates.php
@@ -1,23 +1,41 @@
 <?php
 global $config;
-if(checkloggedin())
+
+// if disabled by admin
+if(!get_option("enable_ai_templates", 1) ) {
+    page_not_found();
+}
+
+if(isset($current_user['id']))
 {
     $start = date('Y-m-01');
     $end = date_create(date('Y-m-t'))->modify('+1 day')->format('Y-m-d');
 
-    $total_words_used = ORM::for_table($config['db']['pre'] . 'word_used')
-        ->where('user_id', $_SESSION['user']['id'])
-        ->where_raw("(`date` BETWEEN '$start' AND '$end')")
-        ->sum('words');
+    $total_words_used = get_user_option($_SESSION['user']['id'], 'total_words_used', 0);
 
-    $membership = get_user_membership_detail($_SESSION['user']['id']);
+    $membership = $current_user['plan'];
     $words_limit = $membership['settings']['ai_words_limit'];
+    $plan_templates = $membership['settings']['ai_templates'];
+
+    $ai_templates = get_ai_templates();
+
+    if(get_option('hide_plan_disabled_features')){
+        foreach ($ai_templates as &$category){
+            $cat_templates = array();
+            foreach ($category['templates'] as $template) {
+                if (in_array($template['slug'], $plan_templates)) {
+                    $cat_templates[] = $template;
+                }
+            }
+            $category['templates'] = $cat_templates;
+        }
+    }
 
     HtmlTemplate::display('ai-templates', array(
-        'ai_templates' => get_ai_templates(),
+        'ai_templates' => $ai_templates,
         'total_words_used' => $total_words_used,
         'words_limit' => $words_limit,
-        'plan_templates' => $membership['settings']['ai_templates']
+        'plan_templates' => $plan_templates
     ));
 }
 else{
