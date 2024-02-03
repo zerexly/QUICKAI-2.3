f4824c15092df111c4ba5ff7295863b653637bae
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-04 00:35:48 +0530

    Update api-keys.php

--- admin/app/api-keys.php
+++ admin/app/api-keys.php
@@ -1,6 +1,55 @@
 <?php
 include '../includes.php';
 
+if (!empty($_GET['refresh-balance'])) {
+    $row = ORM::for_table($config['db']['pre'] . 'api_keys')->find_one($_GET['refresh-balance']);
+
+    if ($row['type'] == 'stable-diffusion') {
+        require_once ROOTPATH . '/includes/lib/StableDiffusion.php';
+
+        $stableDiffusion = new StableDiffusion($row['api_key']);
+        $response = $stableDiffusion->balance();
+        $response = json_decode($response, true);
+
+        $credits = 0;
+        if (isset($response['credits'])) {
+            $credits = round($response['credits'], 2);
+        }
+        $balance = $credits;
+    } else {
+
+        // TODO: disable this right now
+        /*require_once ROOTPATH . '/includes/lib/orhanerday/open-ai/src/OpenAi.php';
+        require_once ROOTPATH . '/includes/lib/orhanerday/open-ai/src/Url.php';
+
+        $open_ai = new Orhanerday\OpenAi\OpenAi($row['api_key']);
+        $response = $open_ai->used_balance();
+        $response = json_decode($response, true);
+
+        $uses = $hard_limit = 0;
+        if (isset($response['total_usage'])) {
+            $uses = round($response['total_usage']) / 100;
+        }
+        $response = $open_ai->balance_hard_limit();
+        $response = json_decode($response, true);
+
+        if (isset($response['hard_limit_usd'])) {
+            $hard_limit = round($response['hard_limit_usd'] * 100) / 100;
+        }
+
+        $balance = "$$uses / $$hard_limit"; */
+        $balance = " - ";
+    }
+
+    /* update balance */
+    $api_key = ORM::for_table($config['db']['pre'] . 'api_keys')->find_one($_GET['refresh-balance']);
+    $api_key->balance = $balance;
+    $api_key->balance_last_update = date('Y-m-d H:i:s');
+    $api_key->save();
+
+    headerRedirect(parse_url(get_current_page_url(), PHP_URL_PATH));
+}
+
 $page_title = __('API Keys');
 include '../header.php'; ?>
 
@@ -37,6 +86,11 @@ include '../header.php'; ?>
                                 <th><?php _e('Title') ?></th>
                                 <th class="no-sort"><?php _e('API Key') ?></th>
                                 <th><?php _e('Type') ?></th>
+                                <th><?php _e('Balance') ?>
+                                    <i class="fa fa-info-circle"
+                                       title="<?php _e('The balance will be automatically refreshed once a day.'); ?>"
+                                       data-tippy-placement="top"></i>
+                                </th>
                                 <th><?php _e('Active') ?></th>
                                 <th width="20" class="no-sort" data-priority="1"></th>
                                 <th width="20" class="no-sort" data-priority="1">
@@ -61,7 +115,8 @@ include '../header.php'; ?>
                 <i class="icon icon-feather-trash-2" aria-hidden="true"></i>
             </button>
         </div>
-        <button type="button" class="front-icon btn btn-primary btn-floating" data-url="panel/api_keys.php" data-toggle="slidePanel">
+        <button type="button" class="front-icon btn btn-primary btn-floating" data-url="panel/api_keys.php"
+                data-toggle="slidePanel">
             <i class="icon-feather-plus animation-scale-up" aria-hidden="true"></i>
         </button>
         <button type="button" class="back-icon btn btn-primary btn-floating">
@@ -69,7 +124,7 @@ include '../header.php'; ?>
         </button>
     </div>
     <script>
-        var QuickMenu = {"page":"api-keys"};
+        var QuickMenu = {"page": "api-keys"};
     </script>
 
 <?php ob_start() ?>
