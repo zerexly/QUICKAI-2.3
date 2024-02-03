e43fa15b5be13cf7779106b6f80710f61364dcbd
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-02-04 00:37:33 +0530

    Update users.php

--- admin/global/users.php
+++ admin/global/users.php
@@ -1,6 +1,50 @@
 <?php
 include '../includes.php';
 
+/* Export users.csv */
+if(isset($_GET['export']) && $_GET['export'] == 'csv'){
+    if(check_allow()) {
+        header('Content-Disposition: attachment; filename="' . 'users.csv";');
+        header('Content-Type: application/csv; charset=UTF-8');
+
+        $users = ORM::for_table($config['db']['pre'] . 'user')->find_array();
+
+        $type_array = ['id', 'username', 'email', 'status', 'created_at'];
+        $result = '';
+
+        /* Export the header */
+        $headers = [];
+        foreach (array_keys((array)reset($users)) as $value) {
+            /* Check if not excluded */
+            if (in_array($value, $type_array)) {
+                $headers[] = '"' . $value . '"';
+            }
+        }
+
+        $result .= implode(',', $headers);
+
+        /* Data */
+        foreach ($users as $row) {
+            $result .= "\n";
+
+            $row_array = [];
+
+            foreach ($row as $key => $value) {
+                /* Check if not excluded */
+                if (in_array($key, $type_array)) {
+                    $row_array[] = '"' . addslashes($value ?? '') . '"';
+                }
+            }
+
+            $result .= implode(',', $row_array);
+        }
+
+        die($result);
+    } else {
+        exit('Access Denied.');
+    }
+}
+
 $page_title = __('Users');
 include '../header.php'; ?>
 
@@ -15,6 +59,11 @@ include '../header.php'; ?>
                     <div class="col-lg-6 main-header">
                         <h2><?php _esc($page_title) ?></h2>
                         <h6 class="mb-0"><?php _e('admin panel') ?></h6>
+                    </div>
+                    <div class="col-lg-6 text-right">
+                        <a href="?export=csv" class="btn btn-primary" target="_blank">
+                            <i class="icon-feather-download"></i> <?php _e('Export CSV') ?>
+                        </a>
                     </div>
                 </div>
             </div>
